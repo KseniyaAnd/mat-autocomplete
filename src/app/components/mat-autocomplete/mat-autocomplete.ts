ebe95b4e38f52604a27b742f39828e2ba4196581
Kseniya <kseniya.andrusenko72@gmail.com>
2025-08-06 21:35:08 +0300

    init: add clear filter and error styles

--- src/app/components/mat-autocomplete/mat-autocomplete.ts
+++ src/app/components/mat-autocomplete/mat-autocomplete.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from '@angular/material/autocomplete';
 import {MatFormField, MatInput} from '@angular/material/input';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {map, Observable, startWith} from 'rxjs';
-import {AsyncPipe} from '@angular/common';
+import {AbstractControl, FormControl, ReactiveFormsModule, ValidationErrors, ValidatorFn} from '@angular/forms';
+import {BehaviorSubject, map, Observable, of, startWith, switchMap} from 'rxjs';
+import {AsyncPipe, NgClass} from '@angular/common';
 import {AutocompleteService} from '../../services/autocomplete-service';
 import {TranslatePipe} from '@ngx-translate/core';
 
@@ -22,7 +22,8 @@ export interface Country {
     MatAutocompleteTrigger,
     AsyncPipe,
     MatInput,
-    TranslatePipe
+    TranslatePipe,
+    NgClass
   ],
   templateUrl: './mat-autocomplete.html',
   styleUrl: './mat-autocomplete.css'
@@ -34,16 +35,50 @@ export class Autocomplete implements OnInit {
     this.Countries = autocompleteService.Countries
   }
 
-  myControl = new FormControl();
+  myControl: FormControl = new FormControl('');
+  private showAll$ = new BehaviorSubject<boolean>(false);
   filteredOptions!: Observable<Country[]>;
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(""),
-      map(value => (typeof value === "string" ? value : value.name)),
-      map(name => (name ? this.autocompleteService.filterCountry(name) : this.Countries.slice()))
+    this.myControl.setValidators(this.validateCountry(this.Countries));
+
+    this.filteredOptions = this.showAll$.pipe(
+      switchMap(showAll => {
+        if (showAll) {
+          // Показываем все страны без фильтра
+          return of(this.Countries.slice());
+        } else {
+          // Фильтруем по введенному значению
+          return this.myControl.valueChanges.pipe(
+            startWith(this.myControl.value || ''),
+            map(value => (typeof value === 'string' ? value : value?.name)),
+            map(name =>
+              name?.trim()
+                ? this.autocompleteService.filterCountry(name)
+                : this.Countries.slice()
+            )
+          );
+        }
+      })
     );
   }
 
+  onFocus() {
+    // При фокусе включаем показ всех вариантов
+    this.showAll$.next(true);
+  }
 
+  onBlur() {
+    // При потере фокуса снова включаем фильтрацию по вводу
+    this.showAll$.next(false);
+  }
+
+
+  validateCountry(countries: Country[]): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const inputValue = control.value;
+      const match = countries.some(country => country.name.toLowerCase() === inputValue?.toLowerCase());
+      return match ? null : { invalidCountry: true };
+    };
+  }
 }
